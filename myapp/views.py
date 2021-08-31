d42bc4d5e059b05a76e64545edd247d156a4d0d2
juhyeok99 <50165225+juhyeok99@users.noreply.github.com>
2021-08-31 21:35:09 +0900

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -7,16 +7,14 @@ def home(request):
 def exe_open(request):
     present_path = os.getcwd()
     path_list = present_path.split(os.path.sep)
-    exe_path = os.path.join(path_list[0], '\\', path_list[1], path_list[2], path_list[3],
-                            "pythonProject1", "mouse-cursor-control", "mouse-cursor-control.exe")
+    exe_path = os.path.join(os.path.abspath('mouse-cursor-control'), 'mouse-cursor-control.exe')
     os.popen(exe_path)
     return render(request, 'myapp/home.html')
 
 def exe_close(request):
     present_path = os.getcwd()
     path_list = present_path.split(os.path.sep)
-    exe_path = os.path.join(path_list[0], '\\', path_list[1], path_list[2], path_list[3],
-                            "pythonProject1", "mouse-cursor-control", "mouse-cursor-control.exe")
+    exe_path = os.path.join(os.path.abspath('mouse-cursor-control'), 'mouse-cursor-control.exe')
     os.system('taskkill /f /im mouse-cursor-control.exe')
     return render(request, 'myapp/home.html')
 
